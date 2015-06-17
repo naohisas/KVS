11775d36a0aed102c5dd96b92cede181fd190ebe
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2015-06-17 23:38:27 +0900

    Fixed a problem for FCC.

--- Source/Core/Utility/StaticAssert.h
+++ Source/Core/Utility/StaticAssert.h
@@ -16,6 +16,8 @@
   #else
     #define KVS_STATIC_ASSERT( expr, mes ) static_assert( expr, mes )
   #endif
+#elif defined KVS_COMPILER_FCC
+  #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ]
 #else
   #error "Not Supported Compiler"
 #endif
