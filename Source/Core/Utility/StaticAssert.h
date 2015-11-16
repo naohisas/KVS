f6b73f5cd7aeeafdf1320d9676b3e693334c59db
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2015-11-16 17:58:59 +0900

    Fixed a problem.

--- Source/Core/Utility/StaticAssert.h
+++ Source/Core/Utility/StaticAssert.h
@@ -3,12 +3,18 @@
 
 
 #if defined KVS_COMPILER_GCC
-  #if KVS_COMPILER_VERSION_LESS_THAN( 3, 1 )
-    #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ]
-  #elif KVS_COMPILER_VERSION_LESS_THAN( 4, 9 )
-    #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ] __attribute__((unused))
+  #if !defined(__cpp_static_assert) && !defined(static_assert)
+    #if KVS_COMPILER_VERSION_LESS_THAN( 3, 1 )
+      #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ]
+    #else
+      #define KVS_STATIC_ASSERT( expr, mes ) typedef char KVS_STATIC_ASSERTION_FAILURE[ ( expr ) ? 1 : -1 ] __attribute__((unused))
+    #endif
   #else
-    #define KVS_STATIC_ASSERT( expr, mes ) static_assert( expr, mes )
+    #if defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7))) || (defined(__apple_build_version__) && (__apple_build_version__ >= 7000000))
+      #define KVS_STATIC_ASSERT( expr, mes ) static_assert( expr, mes ) __attribute__((unused))
+    #else
+      #define KVS_STATIC_ASSERT( expr, mes ) static_assert( expr, mes )
+    #endif
   #endif
 #elif defined KVS_COMPILER_VC
   #if KVS_COMPILER_VERSION_LESS_THAN( 10, 0 )
