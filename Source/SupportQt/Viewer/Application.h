f31f87c8c8b9bf400a6492de58b2e5463a15764f
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2018-12-11 14:36:54 +0900

    Modified for Qt 5.12 (Removed GLUT functions)

--- Source/SupportQt/Viewer/Application.h
+++ Source/SupportQt/Viewer/Application.h
@@ -12,9 +12,7 @@
  *  $Id: Application.h 1443 2013-03-18 07:00:28Z naohisa.sakamoto@gmail.com $
  */
 /*****************************************************************************/
-#ifndef KVS__QT__APPLICATION_H_INCLUDE
-#define KVS__QT__APPLICATION_H_INCLUDE
-
+#pragma once
 #include <kvs/ClassName>
 #include <kvs/ApplicationBase>
 #include <kvs/qt/Qt>
@@ -38,24 +36,16 @@ class Application : public kvs::ApplicationBase
     kvsClassName( kvs::qt::Application );
 
 private:
-
     QApplication* m_app; ///< Qt application
 
 public:
-
     Application( int& argc, char** argv );
+    virtual ~Application();
 
-    virtual ~Application( void );
-
-public:
-
-    virtual int run( void );
-
-    virtual void quit( void );
+    virtual int run();
+    virtual void quit();
 };
 
 } // end of namespace qt
 
 } // end of namespace kvs
-
-#endif // KVS__QT__APPLICATION_H_INCLUDE
