9c3eccf80305116b9c09a92d9b5e034ac43c513a
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2020-01-14 22:02:24 +0900

    Fixed a problem related to DPR.

--- Source/Core/Visualization/Renderer/Axis2D.cpp
+++ Source/Core/Visualization/Renderer/Axis2D.cpp
@@ -111,7 +111,8 @@ void Axis2D::exec( kvs::ObjectBase* object, kvs::Camera* camera, kvs::Light* lig
         {
             kvs::OpenGL::PushMatrix();
             const float y_value_position = kvs::Math::Min( y_min_position.x(), y_max_position.x() );
-            kvs::OpenGL::Translate( y_value_position - 5.0f, ( y0 + y1 + metrics.width( y_label ) ) * 0.5f, 0.0f );
+//            kvs::OpenGL::Translate( y_value_position - 5.0f, ( y0 + y1 + metrics.width( y_label ) ) * 0.5f, 0.0f );
+            kvs::OpenGL::Translate( y_value_position * dpr - 5.0f, ( y0 + y1 + metrics.width( y_label ) ) * 0.5f * dpr, 0.0f );
             kvs::OpenGL::Rotate( -90.0, 0, 0, 1 );
             m_painter.drawText( kvs::Vec2( 0, 0 ), y_label );
             kvs::OpenGL::PopMatrix();
