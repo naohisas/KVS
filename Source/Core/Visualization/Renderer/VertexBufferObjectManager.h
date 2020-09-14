1dab301d947ed491a1654f4b8c7f352212b09e7c
Martin Andersson <andersson.martin@jaea.go.jp>
2020-09-14 14:36:00 +0900

    Give VertexBufferObjectManager the ability to byte align buffers

--- Source/Core/Visualization/Renderer/VertexBufferObjectManager.h
+++ Source/Core/Visualization/Renderer/VertexBufferObjectManager.h
@@ -65,11 +65,13 @@ public:
 public:
     class Binder;
 
+    static GLsizei m_buffer_alignment; //Default initalized to 1
 private:
     kvs::VertexBufferObject m_vbo;
     kvs::IndexBufferObject m_ibo;
     size_t m_vbo_size;
     size_t m_ibo_size;
+
 
     VertexBuffer m_vertex_array;
     VertexBuffer m_color_array;
@@ -97,6 +99,10 @@ public:
     void setIndexArray( const kvs::AnyValueArray& array );
     void setVertexAttribArray( const kvs::AnyValueArray& array, const size_t index, const size_t dim, const bool normalized = false, const size_t stride = 0 );
 
+
+    GLsizei getPaddedBufferSize(VertexBuffer buff) const{
+        return ceil(buff.size / (double)m_buffer_alignment ) * m_buffer_alignment;
+    }
     void create();
     void bind() const;
     void unbind() const;
