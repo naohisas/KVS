7b6df1c4279df74684209ead236d148c30e3a969
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2021-09-21 22:24:44 +0900

    Modified for FCC compiler.

--- Source/SupportPython/NumPy.h
+++ Source/SupportPython/NumPy.h
@@ -9,9 +9,3 @@
 #define PY_ARRAY_UNIQUE_SYMBOL KVS_PYTHON_NUMPY_ARRAYOBJECT_H
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
-
-#include <kvs/Compiler>
-#if defined( KVS_COMPILER_FCC )
-#define __AARCH64EL__ 1
-#define __ARM_64BIT_STATE 1 
-#endif
