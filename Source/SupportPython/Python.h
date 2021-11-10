747c970a128c079b690468fc13198d3b6015359a
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2021-11-10 19:52:50 +0900

    Update Python.h

--- Source/SupportPython/Python.h
+++ Source/SupportPython/Python.h
@@ -9,20 +9,20 @@
 #include <kvs/Compiler>
 
 #if KVS_COMPILER_SUPPORT_CXX11
-  #if defined( __GNUC__ )
-    #pragma GCC diagnostic push
-    #if (KVS_PYTHON_VERSION == 2) && ( __GNUC__ >= 7 )
-      #pragma GCC diagnostic ignored "-Wregister"
-    #endif
-    #include <Python.h>
-    #pragma GCC diagnostic pop
-  #elif defined( __clang__ )
+  #if defined( __clang__ )
     #pragma clang diagnostic push
     #if (KVS_PYTHON_VERSION == 2)
       #pragma clang diagnostic ignored "-Wdeprecated-register"
     #endif
     #include <Python.h>
     #pragma clang diagnostic pop
+  #elif defined( __GNUC__ )
+    #pragma GCC diagnostic push
+    #if (KVS_PYTHON_VERSION == 2) && ( __GNUC__ >= 7 )
+      #pragma GCC diagnostic ignored "-Wregister"
+    #endif
+    #include <Python.h>
+    #pragma GCC diagnostic pop
   #else
     #include <Python.h>
   #endif
