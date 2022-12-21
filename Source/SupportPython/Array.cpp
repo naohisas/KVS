35211f2afe3d7a2c37627a1b5f5c4c17c129f511
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2022-12-21 23:51:38 +0900

    Modified.

--- Source/SupportPython/Array.cpp
+++ Source/SupportPython/Array.cpp
@@ -13,8 +13,14 @@ namespace
 {
 
 template <typename T> int Type() { return NPY_NOTYPE; }
-template <> int Type<kvs::Int32>() { return NPY_INT32; }
-template <> int Type<kvs::Int64>() { return NPY_INT64; }
+template <> int Type<kvs::Int8>()   { return NPY_INT8;    }
+template <> int Type<kvs::Int16>()  { return NPY_INT16;   }
+template <> int Type<kvs::Int32>()  { return NPY_INT32;   }
+template <> int Type<kvs::Int64>()  { return NPY_INT64;   }
+template <> int Type<kvs::UInt8>()  { return NPY_UINT8;   }
+template <> int Type<kvs::UInt16>() { return NPY_UINT16;  }
+template <> int Type<kvs::UInt32>() { return NPY_UINT32;  }
+template <> int Type<kvs::UInt64>() { return NPY_UINT64;  }
 template <> int Type<kvs::Real32>() { return NPY_FLOAT32; }
 template <> int Type<kvs::Real64>() { return NPY_FLOAT64; }
 
@@ -61,8 +67,14 @@ kvs::ValueArray<OUT> Convert( const PyArrayObject* array )
     const int type = PyArray_TYPE( array );
     switch ( type )
     {
+    case NPY_INT8:    { using IN = kvs::Int8;   return ::Convert<IN,OUT>( array ); }
+    case NPY_INT16:   { using IN = kvs::Int16;  return ::Convert<IN,OUT>( array ); }
     case NPY_INT32:   { using IN = kvs::Int32;  return ::Convert<IN,OUT>( array ); }
     case NPY_INT64:   { using IN = kvs::Int64;  return ::Convert<IN,OUT>( array ); }
+    case NPY_UINT8:   { using IN = kvs::UInt8;  return ::Convert<IN,OUT>( array ); }
+    case NPY_UINT16:  { using IN = kvs::UInt16; return ::Convert<IN,OUT>( array ); }
+    case NPY_UINT32:  { using IN = kvs::UInt32; return ::Convert<IN,OUT>( array ); }
+    case NPY_UINT64:  { using IN = kvs::UInt64; return ::Convert<IN,OUT>( array ); }
     case NPY_FLOAT32: { using IN = kvs::Real32; return ::Convert<IN,OUT>( array ); }
     case NPY_FLOAT64: { using IN = kvs::Real64; return ::Convert<IN,OUT>( array ); }
     default:
@@ -89,6 +101,16 @@ bool Array::Check( const kvs::python::Object& object )
         PyArray_NDIM( (const PyArrayObject*)object.get() ) == 1;
 }
 
+Array::Array( const kvs::ValueArray<kvs::Int8>& array ):
+    kvs::python::Object( ::Convert<kvs::Int8>( array ) )
+{
+}
+
+Array::Array( const kvs::ValueArray<kvs::Int16>& array ):
+    kvs::python::Object( ::Convert<kvs::Int16>( array ) )
+{
+}
+
 Array::Array( const kvs::ValueArray<kvs::Int32>& array ):
     kvs::python::Object( ::Convert<kvs::Int32>( array ) )
 {
@@ -96,6 +118,26 @@ Array::Array( const kvs::ValueArray<kvs::Int32>& array ):
 
 Array::Array( const kvs::ValueArray<kvs::Int64>& array ):
     kvs::python::Object( ::Convert<kvs::Int64>( array ) )
+{
+}
+
+Array::Array( const kvs::ValueArray<kvs::UInt8>& array ):
+    kvs::python::Object( ::Convert<kvs::UInt8>( array ) )
+{
+}
+
+Array::Array( const kvs::ValueArray<kvs::UInt16>& array ):
+    kvs::python::Object( ::Convert<kvs::UInt16>( array ) )
+{
+}
+
+Array::Array( const kvs::ValueArray<kvs::UInt32>& array ):
+    kvs::python::Object( ::Convert<kvs::UInt32>( array ) )
+{
+}
+
+Array::Array( const kvs::ValueArray<kvs::UInt64>& array ):
+    kvs::python::Object( ::Convert<kvs::UInt64>( array ) )
 {
 }
 
@@ -112,6 +154,32 @@ Array::Array( const kvs::ValueArray<kvs::Real64>& array ):
 Array::Array( const kvs::python::Object& value ):
     kvs::python::Object( value )
 {
+}
+
+Array::operator kvs::ValueArray<kvs::Int8>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::Int8>(); // empyt array
+    }
+
+    return ::Convert<kvs::Int8>( array );
+}
+
+Array::operator kvs::ValueArray<kvs::Int16>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::Int16>(); // empyt array
+    }
+
+    return ::Convert<kvs::Int16>( array );
 }
 
 Array::operator kvs::ValueArray<kvs::Int32>() const
@@ -138,6 +206,58 @@ Array::operator kvs::ValueArray<kvs::Int64>() const
     }
 
     return ::Convert<kvs::Int64>( array );
+}
+
+Array::operator kvs::ValueArray<kvs::UInt8>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::UInt8>(); // empyt array
+    }
+
+    return ::Convert<kvs::UInt8>( array );
+}
+
+Array::operator kvs::ValueArray<kvs::UInt16>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::UInt16>(); // empyt array
+    }
+
+    return ::Convert<kvs::UInt16>( array );
+}
+
+Array::operator kvs::ValueArray<kvs::UInt32>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::UInt32>(); // empyt array
+    }
+
+    return ::Convert<kvs::UInt32>( array );
+}
+
+Array::operator kvs::ValueArray<kvs::UInt64>() const
+{
+    const auto* array = (const PyArrayObject*)get();
+    const int ndim = PyArray_NDIM( array );
+    if ( ndim != 1 )
+    {
+        kvsMessageError() << "PyArray is not array." << std::endl;
+        return kvs::ValueArray<kvs::UInt64>(); // empyt array
+    }
+
+    return ::Convert<kvs::UInt64>( array );
 }
 
 Array::operator kvs::ValueArray<kvs::Real32>() const
--- Source/SupportPython/Array.h
+++ Source/SupportPython/Array.h
@@ -22,14 +22,26 @@ public:
     static bool Check( const kvs::python::Object& object );
 
 public:
-    Array( const kvs::ValueArray<kvs::Int32>& array );
-    Array( const kvs::ValueArray<kvs::Int64>& array );
+    Array( const kvs::ValueArray<kvs::Int8>&   array );
+    Array( const kvs::ValueArray<kvs::Int16>&  array );
+    Array( const kvs::ValueArray<kvs::Int32>&  array );
+    Array( const kvs::ValueArray<kvs::Int64>&  array );
+    Array( const kvs::ValueArray<kvs::UInt8>&  array );
+    Array( const kvs::ValueArray<kvs::UInt16>& array );
+    Array( const kvs::ValueArray<kvs::UInt32>& array );
+    Array( const kvs::ValueArray<kvs::UInt64>& array );
     Array( const kvs::ValueArray<kvs::Real32>& array );
     Array( const kvs::ValueArray<kvs::Real64>& array );
     Array( const kvs::python::Object& array );
 
+    operator kvs::ValueArray<kvs::Int8>() const;
+    operator kvs::ValueArray<kvs::Int16>() const;
     operator kvs::ValueArray<kvs::Int32>() const;
     operator kvs::ValueArray<kvs::Int64>() const;
+    operator kvs::ValueArray<kvs::UInt8>() const;
+    operator kvs::ValueArray<kvs::UInt16>() const;
+    operator kvs::ValueArray<kvs::UInt32>() const;
+    operator kvs::ValueArray<kvs::UInt64>() const;
     operator kvs::ValueArray<kvs::Real32>() const;
     operator kvs::ValueArray<kvs::Real64>() const;
 };
