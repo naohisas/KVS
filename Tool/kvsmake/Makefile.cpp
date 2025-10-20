7d32ea393dce3c565346f0261899cb9566baf982
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2025-10-20 18:47:48 +0900

    Added a new definition "KVS_USE_MPI" to kvsmake.

--- Tool/kvsmake/Makefile.cpp
+++ Tool/kvsmake/Makefile.cpp
@@ -60,10 +60,15 @@ void Write( std::ifstream& in, std::ofstream& out, const std::string& project_na
 #endif
         if ( use_mpi )
         {
+            line = kvs::String::Replace( line, "USE_MPI_REPLACED_BY_KVSMAKE", "-DKVS_USE_MPI" );
             line = kvs::String::Replace( line, "(CPP)", "(MPICPP)" );
             line = kvs::String::Replace( line, "(CC)", "(MPICC)" );
             line = kvs::String::Replace( line, "(FC)", "(MPIFC)" );
             line = kvs::String::Replace( line, "(LD)", "(MPILD)" );
+        }
+        else
+        {
+            line = kvs::String::Replace( line, "USE_MPI_REPLACED_BY_KVSMAKE", "" );
         }
         out << line << std::endl;
     }
